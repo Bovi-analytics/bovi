bbec31bef8ae2242459f2c0944b045cabb1a1bd4
Douwe de Kok <douwedekok@gmail.com>
2026-05-27 15:40:20 +0200

    docs: render characteristics modules separately

--- packages/models/lactationcurve/docs.py
+++ packages/models/lactationcurve/docs.py
@@ -5,5 +5,7 @@ import pdoc
 pdoc.pdoc(
     "src/lactationcurve",
     "lactationcurve.characteristics.best_predict",
+    "lactationcurve.characteristics.lactation_curve_characteristics",
+    "lactationcurve.characteristics.method_test_interval",
     output_directory=Path("docs"),
 )
